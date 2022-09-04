c9bb759d0b1cbc82e5203012a8df62388983a4a7
Carlos J. Ramirez <tomkat_cr@yahoo.com>
2022-09-03 21:33:28 -0500

    Add: main smart contract development began... Add: unit test began...

--- mine-backend/contracts/Mine.sol
+++ mine-backend/contracts/Mine.sol
@@ -1,12 +1,125 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
-// Import this file to use console.log
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Mine {
-  
-    constructor() {
+contract Mine is ERC721, ERC721URIStorage, AccessControl {
+    
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    string public constant CONTRACT_NAME = "Mine";
+    string public constant CONTRACT_SYMBOL = "MINE";
+    string public constant CONTRACT_VERSION = "0.0.1";
+
+    bytes32 public constant CERTIFIER_ROLE = keccak256("CERTIFIER_ROLE");
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
+
+    enum FeeType {
+        Certifier_Registration,     // 0
+        User_Registration,          // 1
+        Product_Registration,       // 2
+        Product_Transfer            // 3
     }
 
+    mapping (address => string) public certifiers;
+    mapping (uint256 => uint256) public productsPrice;
+    mapping (address => bool) public bannedUsers;
+    mapping (FeeType => uint256) public fees;
+
+    constructor() ERC721(CONTRACT_NAME, CONTRACT_SYMBOL) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function safeMint(address to, string memory metadata_url, uint256 price) public {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, metadata_url);
+        productsPrice[tokenId] = price;
+    
+    }
+
+    function updateMetadataURL(uint256 _tokenId, string memory metadata_url) public {
+        // Update the URI
+        _setTokenURI(_tokenId, metadata_url);
+    }
+
+    function getContractVersion() public pure returns (string memory) {
+        return CONTRACT_VERSION;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function banUser(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        bannedUsers[_account] = true;
+    }
+
+    function unbanUser(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        if (bannedUsers[_account]) delete bannedUsers[_account];
+    }
+
+    // BUYER STUFF
+
+    function BuyProduct(uint256 _tokenId) public payable {
+        require(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender, "No puedes comprar siendo Admin"));
+        require(!hasRole(CERTIFIER_ROLE, msg.sender, "No puedes comprar siendo Certifier"));
+
+        // Token transference missing
+
+        ownerOf(_tokenId).transfer(msg.value);
+    }
+
+    // CERTIFIER STUFF
+
+    function certify(uint256 _tokenId, string memory newMetadata) onlyRole(CERTIFIER_ROLE) public {
+        updateMetadataURL(_tokenId, newMetadata);
+    }
+
+    // function registerAsCertifier(string memory certifierDataURL) public {
+    //     require(msg.value == getAmountToPay(fees[FeeType.Certifier_Registration]), "Hermano paga lo que es");
+    //     require(!hasRole(DEFAULT_ADMIN_ROLE), "Admin cannot be certifier");
+    //     certifiers[msg.sender] = certifierDataURL;
+    //     owner.transfer(msg.value);
+    // }
+
+    function addCertifier(address _account) onlyRole(DEFAULT_ADMIN_ROLE) public {
+        _grantRole(CERTIFIER_ROLE, _account);
+    }
+
+    function removeCertifier(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(CERTIFIER_ROLE, _account);
+    }
+
+    // FEE STUFF
+    
+    function setFee(FeeType feeType, uint256 _feeAmount) onlyRole(DEFAULT_ADMIN_ROLE) public {
+        fees[feeType] = _feeAmount;
+    }
 }
