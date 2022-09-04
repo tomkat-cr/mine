759c535008924d80f1326a318ff0ad2ff6a12fe5
Eimer Castro <Eimer.Castro@nordstrom.com>
2022-09-04 12:00:32 -0500

    Fixing buy and approval functions and adding file to debug.

--- mine-backend/contracts/Mine.sol
+++ mine-backend/contracts/Mine.sol
@@ -28,32 +28,45 @@ contract Mine is ERC721, ERC721URIStorage, AccessControl {
     }
 
     mapping (address => string) public certifiers;
+    mapping (address => bool) public reviewedCertifiers;
     mapping (uint256 => uint256) public productsPrice;
     mapping (address => bool) public bannedUsers;
     mapping (FeeType => uint256) public fees;
+    mapping (uint256 => address) public tokenIdToUser;
+
+    /*
+    fees[Certifier_Registration] = 1;   // Fixed amount Gwei
+    fees[User_Registration] = 0;        // Fixed amount Gwei
+    fees[Product_Registration] = 1;     // Fixed amount Gwei
+    fees[Product_Transfer] = 1;         // Percentage
+    */
 
     constructor() ERC721(CONTRACT_NAME, CONTRACT_SYMBOL) {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         contractOwner = payable(msg.sender);
     }
 
-    function safeMint(address to, string memory metadata_url, uint256 price) public payable {
-        contractOwner.transfer(fees[FeeType.Product_Registration]);
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, metadata_url);
-        productsPrice[tokenId] = price;
-    
-    }
-
-    function updateMetadataURL(uint256 _tokenId, string memory metadata_url) public {
-        // Update the URI
-        _setTokenURI(_tokenId, metadata_url);
-    }
+    // Contract functions
 
     function getContractVersion() public pure returns (string memory) {
         return CONTRACT_VERSION;
+    }
+
+    // NFT functions
+
+    function safeMint(address _to, string memory _metadataUrl, uint256 _price) public payable {
+        contractOwner.transfer(fees[FeeType.Product_Registration]);
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(_to, tokenId);
+        _setTokenURI(tokenId, _metadataUrl);
+        productsPrice[tokenId] = _price;
+        tokenIdToUser[tokenId] = msg.sender;
+    }
+
+    function updateMetadataURL(uint256 _tokenId, string memory _metadataUrl) public {
+        // Update the URI
+        _setTokenURI(_tokenId, _metadataUrl);
     }
 
     // The following functions are overrides required by Solidity.
@@ -80,6 +93,8 @@ contract Mine is ERC721, ERC721URIStorage, AccessControl {
         return super.supportsInterface(interfaceId);
     }
 
+    // Banning
+
     function banUser(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         bannedUsers[_account] = true;
     }
@@ -88,38 +103,62 @@ contract Mine is ERC721, ERC721URIStorage, AccessControl {
         if (bannedUsers[_account]) delete bannedUsers[_account];
     }
 
-    // BUYER STUFF
+    // Buyer functions
 
-    function buyProduct(uint256 _tokenId) public payable {
-        require(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "No puedes comprar siendo Admin");
-        require(!hasRole(CERTIFIER_ROLE, msg.sender), "No puedes comprar siendo Certifier");
+    function buyProduct(uint256 _tokenId, uint256 _gofPortion) public payable {
+        require(_tokenId < _tokenIdCounter.current(), "Invalid tokenId.");
+        require(hasRole(USER_ROLE, msg.sender), "You should be an user to buy this product.");
+        require(_isApprovedOrOwner(_tokenId) != msg.sender, "You cannot buy your own products.");
+        require(productPrice(_tokenId) != msg.value, "Transaction amount should be the same as the product price.");
 
-        uint256 gofPortion = msg.value * fees[FeeType.Product_Transfer] / 100;
-        contractOwner.transfer(gofPortion);
-        payable(ownerOf(_tokenId)).transfer(msg.value - gofPortion);
+        // uint256 _gofPortion = msg.value * fees[FeeType.Product_Transfer] / 100;
+        contractOwner.transfer(_gofPortion);
+        payable(_isApprovedOrOwner(_tokenId)).transfer(msg.value - _gofPortion);
         // Token transference missing maybe we could find something in:
         // https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/ERC721Tradable.sol
     }
 
-    // CERTIFIER STUFF
+    /* function ownerOf(uint256 _tokenId) private pure  returns (address) {
+        return tokenIdToUser[_tokenId];
+    } */
 
-    function certify(uint256 _tokenId, string memory newMetadata) onlyRole(CERTIFIER_ROLE) public {
-        updateMetadataURL(_tokenId, newMetadata);
+    function _isApprovedOrOwner(uint256 _tokenId) internal view virtual returns (address) {
+        require(tokenIdToUser[_tokenId] == address(0x0), "TokenId doesn't exist.");
+        // address owner = ERC721.ownerOf(tokenId);
+        // return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+        return tokenIdToUser[_tokenId];
     }
 
-    function registerAsCertifier(string memory certifierDataURL) public payable {
-        require(msg.value == fees[FeeType.Certifier_Registration], "Hermano paga lo que es");
-        require(!hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Admin cannot be certifier");
-        certifiers[msg.sender] = certifierDataURL;
+    function productPrice(uint256 _tokenId) private view returns (uint256) {
+        return productsPrice[_tokenId];
+    }
+
+    // Certifier functions
+
+    function certify(uint256 _tokenId, string memory _newMetadata) onlyRole(CERTIFIER_ROLE) public {
+        updateMetadataURL(_tokenId, _newMetadata);
+    }
+
+    function registerAsCertifier(string memory _certifierDataURL) public payable {
+        require(msg.value == fees[FeeType.Certifier_Registration], "Value amount is different than the certifier's registration fee.");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Admin cannot be a certifier.");
+        require(hasRole(USER_ROLE, msg.sender), "Users cannot be a certifier.");
+        certifiers[msg.sender] = _certifierDataURL;
+        reviewedCertifiers[msg.sender] = false;
         contractOwner.transfer(msg.value);
+        // New certifier must be reviewed by GOF, then GOF will call addCertifier()
     }
 
-    function addCertifier(address _account) onlyRole(DEFAULT_ADMIN_ROLE) public {
+    function acceptCertifier(address _account) onlyRole(DEFAULT_ADMIN_ROLE) public {
         _grantRole(CERTIFIER_ROLE, _account);
+        reviewedCertifiers[_account] = true;
     }
 
     function removeCertifier(address _account) public onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(bytes(certifiers[_account]).length > 0, "Certifier doesn't exist.");
         _revokeRole(CERTIFIER_ROLE, _account);
+        delete reviewedCertifiers[_account];
+        delete certifiers[_account];
     }
 
     // FEE STUFF
--- mine-backend/test/Mine.js
+++ mine-backend/test/Mine.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Mine Contract", function () {
+describe("Mine Contract", () => {
 	let Mine;
 	let mine;
 	let admin;
@@ -13,7 +13,7 @@ describe("Mine Contract", function () {
 	let CERTIFIER_ROLE;
 	let USER_ROLE;
 
-	beforeEach(async function () {
+	beforeEach(async () => {
 		Mine = await ethers.getContractFactory("Mine");
 		[admin, certifier, sellerUser, buyerUser, ...addrs] = await ethers.getSigners();
 		mine = await Mine.deploy();
@@ -22,23 +22,23 @@ describe("Mine Contract", function () {
 		USER_ROLE = await mine.USER_ROLE();
 	});
 
-	describe("Role Assignments", function () {
-		it("ADMIN can add Role to a Certifier", async function () {
+	describe("Role Assignments", () => {
+		it("ADMIN can add Role to a Certifier", async () => {
 			await mine.grantRole(CERTIFIER_ROLE, certifier.address);
 			expect(await mine.hasRole(CERTIFIER_ROLE, certifier.address)).to.equal(true);
 		});
 
-		it("ADMIN can remove Role from a Certifier", async function () {
+		it("ADMIN can remove Role from a Certifier", async () => {
 			await mine.grantRole(CERTIFIER_ROLE, certifier.address);
 			await mine.revokeRole(CERTIFIER_ROLE, certifier.address);
 			expect(await mine.hasRole(CERTIFIER_ROLE, certifier.address)).to.equal(false);
 		});
 
-		it("non-ADMIN can't add Role from a Certifier", async function () {
+		it("non-ADMIN can't add Role from a Certifier", async () => {
 			expect(mine.connect(certifier).grantRole(CERTIFIER_ROLE, sellerUser.address)).to.be.reverted;
 		});
 
-		it("non-ADMIN can't remove Role from an Certifier", async function () {
+		it("non-ADMIN can't remove Role from an Certifier", async () => {
 			await mine.grantRole(DEFAULT_ADMIN_ROLE, admin.address);
 			expect(
 			mine.connect(sellerUser).revokeRole(DEFAULT_ADMIN_ROLE, admin.address)
@@ -46,28 +46,35 @@ describe("Mine Contract", function () {
 		});
 	});
 
-	describe("Banning", function () {
-		it("Admin can ban an address", async function () {
+	describe("Banning", () => {
+		it("Admin can ban an address", async () => {
 			await mine.grantRole(DEFAULT_ADMIN_ROLE, admin.address);
 			await mine.connect(admin).banUser(certifier.address);
 			expect(await mine.bannedUsers(certifier.address)).to.be.equal(true);
 		});
 
-		it("Admin can unban an address", async function () {
+		it("Admin can unban an address", async () => {
 			await mine.grantRole(DEFAULT_ADMIN_ROLE, admin.address);
 			await mine.connect(admin).banUser(certifier.address);
 			await mine.connect(admin).unbanUser(certifier.address);
 			expect(await mine.bannedUsers(certifier.address)).to.be.equal(false);
 		});
 
-		it("non-Admin can't ban an address", async function () {
+		it("non-Admin can't ban an address", async () => {
 			await expect(mine.connect(certifier).banUser(sellerUser.address)).to.be.reverted;
 		});
 
-		it("non-Admin can't unban an address", async function () {
+		it("non-Admin can't unban an address", async () => {
 			await mine.grantRole(DEFAULT_ADMIN_ROLE, admin.address);
 			await mine.connect(admin).banUser(certifier.address);
 			await expect(mine.connect(buyerUser).unbanUser(certifier.address)).to.be.reverted;
+		});
+	});
+
+	describe("Mine version", () => {
+		it("Returns Mine version", async () => {
+			const version = await mine.connect(admin).getContractVersion();
+			expect(version).to.equal("0.0.1");
 		});
 	});
 });
