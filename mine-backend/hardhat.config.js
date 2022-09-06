fb48c9da557d48eb7ffcea982eb5d12ff4cb9167
Jose Valentin Salina <josevalentinsp@gmail.com>
2022-09-05 20:06:29 -0400

    fixing goerli url

--- mine-backend/hardhat.config.js
+++ mine-backend/hardhat.config.js
@@ -11,41 +11,9 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    rinkeby: {
-      // Target URL when Hardhat deploy the Smart Contract
-      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID_API_KEY}`,
-      // Indicates which account will sign the transaction
-      accounts: [
-        DEPLOYER_SIGNER_PRIVATE_KEY
-      ]
-    },
-    ropsten: {
-      // Target URL when Hardhat deploy the Smart Contract
-      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID_API_KEY}`,
-      // Indicates which account will sign the transaction
-      accounts: [
-        DEPLOYER_SIGNER_PRIVATE_KEY
-      ]
-    },
     goerli: {
       // Target URL when Hardhat deploy the Smart Contract
-      url: `https://eth-goerli.g.infura.com/v2/${INFURA_PROJECT_ID_API_KEY}`,
-      // Indicates which account will sign the transaction
-      accounts: [
-        DEPLOYER_SIGNER_PRIVATE_KEY
-      ]
-    },
-    kovan: {
-      // Target URL when Hardhat deploy the Smart Contract
-      url: `https://kovan.infura.io/v3/${INFURA_PROJECT_ID_API_KEY}`,
-      // Indicates which account will sign the transaction
-      accounts: [
-        DEPLOYER_SIGNER_PRIVATE_KEY
-      ]
-    },
-    sepolia: {
-      // Target URL when Hardhat deploy the Smart Contract
-      url: `https://sepolia.infura.io/v3/${INFURA_PROJECT_ID_API_KEY}`,
+      url: `https://goerli.infura.io/v3/${INFURA_PROJECT_ID_API_KEY}`,
       // Indicates which account will sign the transaction
       accounts: [
         DEPLOYER_SIGNER_PRIVATE_KEY
